7d3662d2bbfbd83aeade1147f4eac0379a8c7a12
namistarr <namistarr@icloud.com>
2019-08-12 13:25:19 -0700

    yarn install and fixed bugs -MVP

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import CartContext from '../contexts/CartContext';
+import { CartContext } from '../contexts/CartContext';
 
 
 
 const Navigation = () => {
 	const {cart} = useContext(CartContext);
-	
+
 	return (
 		<div className="navigation">
 			<NavLink to="/">Products</NavLink>
